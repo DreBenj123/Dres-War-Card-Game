b33f1962c38a7a2ea99119dff16e99b3e7577f13
DreBenj123 <100318209+DreBenj123@users.noreply.github.com>
2022-06-07 21:13:39 -0400

    end of day 3 commit

--- cards.js
+++ cards.js
@@ -6,6 +6,18 @@ export default class Deck {
     constructor(cards = freshDeck()){
         this.cards=cards
     }
+    get numberOfCards(){
+        return this.cards.length
+    }
+    // Creating Random deck shuffle
+    shuffle(){
+        for (let i= this.numberOfCards - 1; i > 0; i-- ){
+            const newIndex = Math.floor (Math.random() * (i + 1))
+            const oldValue = this.cards[i]
+            this.cards[newIndex] = this.cards[i]
+            this.cards [i] = oldValue
+        }
+    }
 }
 class Card{
     constructor(suit,value){
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import Deck from "./cards.js"
 
 const deck = new Deck()
+deck.shuffle()
 console.log(deck.cards)
