e7c24c82873140345142e00fbeb9b666026a33de
DreBenj123 <100318209+DreBenj123@users.noreply.github.com>
2022-06-09 21:29:16 -0400

    class 4 end commit
    some added JS functionality

--- cards.js
+++ cards.js
@@ -1,38 +1,61 @@
 //creating the deck
-const SUITS =["♥","♦","♠","♣"];
-const VALUES = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+const SUITS = ["♥", "♦", "♠", "♣"];
+const VALUES = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+];
 
 export default class Deck {
-    constructor(cards = freshDeck()){
-        this.cards=cards
+  constructor(cards = freshDeck()) {
+    this.cards = cards;
+  }
+  get numberOfCards() {
+    return this.cards.length;
+  }
+  // Creating Random deck shuffle
+  shuffle() {
+    for (let i = this.numberOfCards - 1; i > 0; i--) {
+      const newIndex = Math.floor(Math.random() * (i + 1));
+      const oldValue = this.cards[i];
+      this.cards[newIndex] = this.cards[i];
+      this.cards[i] = oldValue;
     }
-    get numberOfCards(){
-        return this.cards.length
-    }
-    // Creating Random deck shuffle
-    shuffle(){
-        for (let i= this.numberOfCards - 1; i > 0; i-- ){
-            const newIndex = Math.floor (Math.random() * (i + 1))
-            const oldValue = this.cards[i]
-            this.cards[newIndex] = this.cards[i]
-            this.cards [i] = oldValue
-        }
-    }
+  }
 }
-class Card{
-    constructor(suit,value){
-        this.suit= suit
-        this.value = value
-    }
+//Card In Play
+class Card {
+  constructor(suit, value) {
+    this.suit = suit;
+    this.value = value;
+  }
+  get color() {
+    return this.suit === "♣" || this.suit === "♠" ? "black" : "red";
+  }
+
+  getHTML() {
+    const cardDiv = document.createElement("div");
+    cardDiv.innerText = this.suit;
+    cardDiv.classList.add("card", this.color);
+    cardDiv.dataset.value = `${this.value} ${this.suit}`;
+    return cardDiv;
+  }
 }
 
-function freshDeck(){
-    return SUITS.flatMap(suit=> {
-    return VALUES.map(value=>{
-     return new Card(suit,value)   
-    })    
-
-    })
-
-
+function freshDeck() {
+  return SUITS.flatMap((suit) => {
+    return VALUES.map((value) => {
+      return new Card(suit, value);
+    });
+  });
 }
--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-import Deck from "./cards.js"
+import Deck from "./cards.js";
 
-const deck = new Deck()
-deck.shuffle()
-console.log(deck.cards)
+const computerCardSlot = document.querySelector(".computer-card-slot");
+const playerCardSlot = document.querySelector(".player-card-slot");
+const computerDeckElement = document.querySelector(".computer-deck");
+const playerDeckElement = document.querySelector(".player-deck");
+const text = document.querySelector(".text");
+
+let playerDeck, computerDeck;
+
+startGame();
+function startGame() {
+  const deck = new Deck();
+  deck.shuffle();
+
+  //splitting the deck between players
+  const deckMidpoint = Math.ceil(deck.numberOfCards / 2);
+  playerDeck = new Deck(deck.cards.slice(0, deckMidpoint));
+  computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards));
+}
+//New round clean up
+newGame();
+
+function newGame() {
+  computerCardSlot.innerHTML = "";
+  playerCardSlot.innerHTML = "";
+  text.innerText = "";
+}
+
+updateDeckCount();
+
+function updateDeckCount() {
+  computerDeckElement.innerText = computerDeck.numberOfCards;
+  playerDeckElement.innerText = playerDeck.numberOfCards;
+}
